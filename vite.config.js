3c355999d9e6dde588d1c40f0d45993b97e79e5a
Israel Ayiku <hebronblay@gmail.com>
2026-01-05 14:07:37 +0000

    Update main with latest frontend changes

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,19 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        solutions: path.resolve(__dirname, "solutions.html"),
+        marketplace: path.resolve(__dirname, "marketplace.html"),
+        pricing: path.resolve(__dirname, "pricing.html"),
+        impact: path.resolve(__dirname, "impact.html"),
+        manifesto: path.resolve(__dirname, "manifesto.html"),
+        people: path.resolve(__dirname, "people.html"),
+        projects: path.resolve(__dirname, "projects.html"),
+        cookies: path.resolve(__dirname, "cookies.html"),
+        privacy: path.resolve(__dirname, "privacy.html"),
+      },
     },
   },
   resolve: {
