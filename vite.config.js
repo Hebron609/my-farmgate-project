4d03e45dc46ad101c60eda46a124d5f6e66b0016
David Edem Ndanu <davidedemofc@gmail.com>
2026-01-05 13:52:10 +0000

    [Deploy]

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,7 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        about: path.resolve(__dirname, "about.html"),
-        solutions: path.resolve(__dirname, "solutions.html"),
-        marketplace: path.resolve(__dirname, "marketplace.html"),
-        pricing: path.resolve(__dirname, "pricing.html"),
-        impact: path.resolve(__dirname, "impact.html"),
-        manifesto: path.resolve(__dirname, "manifesto.html"),
-        people: path.resolve(__dirname, "people.html"),
-        projects: path.resolve(__dirname, "projects.html"),
-        cookies: path.resolve(__dirname, "cookies.html"),
-        privacy: path.resolve(__dirname, "privacy.html"),
-      },
+      input: path.resolve(__dirname, "index.html"),
     },
   },
   resolve: {
