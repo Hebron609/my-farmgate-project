0b2a40fd6d71c70765285bb9adf88c22f817d2b8
Israel Ayiku <hebronblay@gmail.com>
2026-01-13 17:13:18 +0000

    Update project files

--- AgroTech-vue-website/src/solutions.js
+++ AgroTech-vue-website/src/solutions.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import UnderConstruction from "./components/UnderConstruction.vue";
+//import Solutions from './pages/Solutions.vue'
 import './style.css'
 import { setupApp } from './shared-setup'
 
