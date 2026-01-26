db3c144ab818b2d2ad11ba01ec7bf5489fcef2e9
CEO1 <CEO@MacBook-Pro-2.local>
2026-01-26 08:02:39 +0000

    Fix people.vue member.title show on mobile viewport

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -42,7 +42,7 @@
           ></div>
         </div>
         <p
-          class="md:max-w-[40%] text-xl md:text-xl text-gray-300 mb-4 font-light"
+          class="md:max-w-[40%] text-md md:text-xl text-gray-300 mb-4 font-light"
         >
           Get in touch with FarmGate Africa. We're here to help advance
           agriculture and connect with you.
