668d5aa3702502265de0dbc4a3fa75b24956bdc7
CEO1 <CEO@MacBook-Pro-2.local>
2026-02-05 15:39:29 +0000

    fixing footprint taking us back to html page

--- src/pages/Footprint.vue
+++ src/pages/Footprint.vue
@@ -29,7 +29,7 @@
       >
         <div class="reveal-slide-up">
           <LeafIcon
-            class="mx-auto mb-6 h-16 w-16 text-[#F2CB00]  animate-pulse"
+            class="mx-auto mb-6 h-16 w-16 text-[#F2CB00] animate-pulse"
           />
 
           <h1
@@ -43,7 +43,8 @@
         <p
           class="mt-8 max-w-2xl text-lg font-light leading-relaxed text-gray-400 md:text-lg reveal-slide-up delay-200"
         >
-          We are not just farming, We are engineering thae backbone of Africa's agricultural revolution.
+          We are not just farming, We are engineering thae backbone of Africa's
+          agricultural revolution.
         </p>
 
         <div class="absolute bottom-10 animate-bounce">
@@ -64,52 +65,57 @@
       </div>
     </div>
 
-    <section ref="footprintSection" class="w-full bg-white pb-20 px-6 md:px-12 mt-14">
+    <section
+      ref="footprintSection"
+      class="w-full bg-white pb-20 px-6 md:px-12 mt-14"
+    >
       <div class="mx-auto max-w-[1440px]">
-        <div class="inline-flex ">
-              <a
-                href="/marketplace.html"
-                class="inline-flex group relative overflow-hidden px-4 py-2 rounded-4xl items-center justify-center cursor-pointer bg-gray-50 border border-gray-200 whitespace-nowrap md:mb-10 mb-5"
+        <div class="inline-flex">
+          <a
+            href="/marketplace.html"
+            class="inline-flex group relative overflow-hidden px-4 py-2 rounded-4xl items-center justify-center cursor-pointer bg-gray-50 border border-gray-200 whitespace-nowrap md:mb-10 mb-5"
+          >
+            <span
+              class="flex items-center gap-2 transition-transform duration-300 transform translate-y-0 group-hover:-translate-y-full"
+            >
+              <LeafIcon class="text-[#129C48] w-4 h-4" />
+              <span class="text-xs font-semibold leading-none text-gray-500"
+                >Start growing with us</span
               >
-                <span
-                  class="flex items-center gap-2 transition-transform duration-300 transform translate-y-0 group-hover:-translate-y-full"
-                >
-                  <LeafIcon class="text-[#129C48] w-4 h-4 " />
-                  <span
-                    class="text-xs font-semibold leading-none text-gray-500"
-                    >Start growing with us</span
-                  >
-                </span>
+            </span>
 
-                <span
-                  class="absolute inset-0 flex items-center justify-center w-full h-full gap-2 text-black transition-transform duration-300 transform translate-y-full bg-yellow-400 group-hover:translate-y-0"
-                >
-                  <LeafIcon class="w-4 h-4 text-black " />
-                  <span class="text-xs font-semibold leading-none"
-                    >Start growing with us</span
-                  >
-                </span>
-              </a>
-            </div>
+            <span
+              class="absolute inset-0 flex items-center justify-center w-full h-full gap-2 text-black transition-transform duration-300 transform translate-y-full bg-yellow-400 group-hover:translate-y-0"
+            >
+              <LeafIcon class="w-4 h-4 text-black" />
+              <span class="text-xs font-semibold leading-none"
+                >Start growing with us</span
+              >
+            </span>
+          </a>
+        </div>
 
-            <div class="flex flex-col md:flex-row justify-between md:items-start md:gap-12 mb-22">
-           <div class="md:max-w-2xl reveal-slide-right">
-            
-              <h3 class="font-['Livvic'] text-4xl md:text-5xl font-bold text-black leading-tight">
-                Our Footprint
-              </h3>
-           </div>
+        <div
+          class="flex flex-col md:flex-row justify-between md:items-start md:gap-12 mb-22"
+        >
+          <div class="md:max-w-2xl reveal-slide-right">
+            <h3
+              class="font-['Livvic'] text-4xl md:text-5xl font-bold text-black leading-tight"
+            >
+              Our Footprint
+            </h3>
+          </div>
 
-           <div class="md:max-w-2xl reveal-slide-left">
-               <div class="h-1 w-24 bg-[#F2CB00] mb-8"></div>
-               <p class="text-gray-600 text-lg leading-relaxed font-light">
-                FarmGate Africa & Industries strategically manages its agricultural footprint to
-            balance large-scale economic development with essential social
-            well-being. By operating across the entire agricultural value chain,
-            the organization produces high-quality goods that serve two distinct
-            purposes.
-              </p>
-           </div>
+          <div class="md:max-w-2xl reveal-slide-left">
+            <div class="h-1 w-24 bg-[#F2CB00] mb-8"></div>
+            <p class="text-gray-600 text-lg leading-relaxed font-light">
+              FarmGate Africa & Industries strategically manages its
+              agricultural footprint to balance large-scale economic development
+              with essential social well-being. By operating across the entire
+              agricultural value chain, the organization produces high-quality
+              goods that serve two distinct purposes.
+            </p>
+          </div>
         </div>
 
         <div class="grid grid-cols-1 gap-8 lg:grid-cols-2">
